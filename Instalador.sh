64a81daa7a810690185a48ad21307c7b122f2ba8
PJR1988 <pablo.jimenez.rebollo@gmail.com>
2017-03-26 15:14:38 +0200

    Update Instalador.sh

--- Instalador.sh
+++ Instalador.sh
@@ -1,4 +1,4 @@
-wget -c https://raw.githubusercontent.com/PJR1988/CodeName/master/CodeNum_Source.tar.gz
+wget -c https://raw.githubusercontent.com/PJR1988/CodeNum/master/CodeNum_Source.tar.gz
 tar -xzvf CodeNum_Source.tar.gz
 cd CodeNum
 sh INSTALL.sh
